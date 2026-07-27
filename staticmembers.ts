808791324fbdc1887b23d3be268245306f50c941
HinduKoppala <hindukoppala2006@gmail.com>
2026-07-27 10:28:35 +0530

    Add files via upload
    OOP concepts

---
+++ staticmembers.ts
@@ -0,0 +1,13 @@
+class MathUtility {
+  public static readonly PI: number = 3.14159;
+  public static totalCalculations: number = 0;
+
+  public static calculateArea(radius: number): number {
+    MathUtility.totalCalculations++;
+    return MathUtility.PI * radius * radius;
+  }
+}
+
+console.log(MathUtility.calculateArea(5)); 
+console.log(MathUtility.totalCalculations); 
+// const m = new MathUtility(); // Can call static without new
